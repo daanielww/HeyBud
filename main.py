63935409055e1467a5b75baa75028deac4b51e12
Jawad Braick <jawadb0199@gmail.co>
2018-03-31 16:12:54 -0400

    First Commit

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ import logging
 import jinja2
 import os
 from google.appengine.ext import ndb
+# from google.cloud import texttospeech
+# from google.cloud import speech
+from google.cloud import texttospeech
 from google.appengine.api import users
 
 jinja_environment = jinja2.Environment(
@@ -27,34 +30,38 @@ jinja_environment = jinja2.Environment(
 
 # UserLogin function to be run on every page, so user can be logged in on every page
 # and be able to add bookmarks and log out
-def UserLogin(user_bookmarks_dict=bookmarks_dict):
-    user = users.get_current_user()
-    # if the user is logged in
-    if user:
-        nickname = user.nickname()
-        global nickname
-        # HTML to display username and sign out button
-        logout_url = users.create_logout_url("/")
-        greeting = "<p id='username' >Welcome, {}! <a id='login_link' href='{}'>Sign Out</a></p>".format(nickname, logout_url)
-        # search DataStore for specific user, to see if this is their first login
-        user_entity_query = UserProperties.query(UserProperties.username == nickname).fetch()
-        # if list of user entities returned by query is empty (current user doesn"t exist), make new user entity
-        if user_entity_query == []:
-            # bookmarks made before logging in are added to the user"s list of bookmarks
-            user_bookmarks_dict_json = json.dumps(user_bookmarks_dict)
-            new_user = UserProperties(username=nickname, bookmarks=user_bookmarks_dict_json)
-            new_user.put()
-    # if no user logged in
-    else:
-        # HTML to display sign in button
-        login_url = users.create_login_url("/")
-        greeting = "<a id='login_link' href='{}'>Log in to save your Bookmarks!</a>.".format(login_url)
-    # Dictionary to pass to template to display log in/out url
-    login_dict = {"header": greeting}
-    return login_dict
+# def UserLogin(user_bookmarks_dict=bookmarks_dict):
+#     user = users.get_current_user()
+#     # if the user is logged in
+#     if user:
+#         nickname = user.nickname()
+#         global nickname
+#         # HTML to display username and sign out button
+#         logout_url = users.create_logout_url("/")
+#         greeting = "<p id='username' >Welcome, {}! <a id='login_link' href='{}'>Sign Out</a></p>".format(nickname, logout_url)
+#         # search DataStore for specific user, to see if this is their first login
+#         user_entity_query = UserProperties.query(UserProperties.username == nickname).fetch()
+#         # if list of user entities returned by query is empty (current user doesn"t exist), make new user entity
+#         if user_entity_query == []:
+#             # bookmarks made before logging in are added to the user"s list of bookmarks
+#             user_bookmarks_dict_json = json.dumps(user_bookmarks_dict)
+#             new_user = UserProperties(username=nickname, bookmarks=user_bookmarks_dict_json)
+#             new_user.put()
+#     # if no user logged in
+#     else:
+#         # HTML to display sign in button
+#         login_url = users.create_login_url("/")
+#         greeting = "<a id='login_link' href='{}'>Log in to save your Bookmarks!</a>.".format(login_url)
+#     # Dictionary to pass to template to display log in/out url
+#     login_dict = {"header": greeting}
+#     return login_dict
+
+# if __name__ == '__main__':
+#     run_quickstart()
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        # run_quickstart()
         self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([
